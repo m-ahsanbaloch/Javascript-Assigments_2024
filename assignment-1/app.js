2c19a92e8f3edb746185bc3fce40afc3dc9149a4
ahsansamad <ingeniouslogo@gmail.com>
2024-02-24 18:29:24 +0500

    javascript-Assigment-1

---
+++ assignment-1/app.js
@@ -0,0 +1,81 @@
+        // 24/2/2024
+        // Chapter-1
+// // ============= Q-1 =============
+// alert("Hello user")
+
+// // =========== Q-2 ================
+
+// alert("Error! Please Enter A Valid Password.")
+
+// // ============= Q-3 ===============
+// alert("Welcome To Js Land... \n happy coding!" )
+
+// ============== Q-4 =============
+// alert("Welcome To Js Land...")
+// alert("Happy Coding!")
+        // Chapter-1-End
+
+// =================================
+
+
+
+        // Chapter-2 ///////////////////////////////////////////
+// // ============= Q-1 =============
+// var userName;
+// // // ============= Q-2 =============
+// var myName = "Muhammad Ahsan"
+// // // ============= Q-3 =============
+// // A
+// var message;
+// // B
+// message = "Hello World"
+// // C
+// alert(message)
+// // ============= Q-4 =============
+// var studentName = "Ahsan"
+// var studentAge = "34 Years Old"
+// var certified = "Certified Mobile Application Development"
+// // ============= Q-5 =============
+// var pizza = "Pizza \nPizz \nPiz \nPi \nP"
+// alert(pizza)
+// // ============= Q-6 =============
+// var email = "ingeniouslogo@gmail.com"
+// alert("My email address is " + email)
+// // ============= Q-7 =============
+// var book = "“A smarter way to learn JavaScript”."
+// alert(book)
+// // ============= Q-8 =============
+// document.write("yes i can write HTML content Through JavaScript")
+// // ============= Q-9 =============
+// var pattern = '“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”'
+// alert(pattern)
+        ///////////////// Chapter-2-End ////////////////
+
+
+         // Chapter-3 ///////////////////////////////////////////
+         
+         // // ============= Q-1 =============
+// var age = "i am 34 Years Old"
+// alert(age)
+// // ============= Q-2 =============
+// var visited = "You have visited this site 14 times"
+// alert(visited)
+// // ============= Q-3 =============
+// var birthYear = 1989
+// document.write("My Brith Year Is " + birthYear + "</br>" + "Data type Of my declared variable is Number")
+// // ============= Q-4 =============
+// A
+// var visitorName = "<b>Muhammad Ahsan </b>"
+// // B
+// var pTitle = "<b>T-Shirt</b>"
+// // C
+// var ordered = "<b>5</b>"
+// document.write(visitorName , "Ordered ", ordered + " " + pTitle + " " + "Ahsan Clothing Store "  )
+
+
+
+
+
+
+
+
