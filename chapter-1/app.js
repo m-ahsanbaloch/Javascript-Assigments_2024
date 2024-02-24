f04b5fdb64a0998cb44f7b26d4f8d46a4d5e2ee1
ahsansamad <ingeniouslogo@gmail.com>
2024-02-24 10:49:57 +0500

    Alhumdulillah Started Javascript

---
+++ chapter-1/app.js
@@ -0,0 +1,13 @@
+// // ============= Q-1 =============
+// alert("Hello user")
+
+// // =========== Q-2 ================
+
+// alert("Error! Please Enter A Valid Password.")
+
+// // ============= Q-3 ===============
+// alert("Welcome To Js Land... \n happy coding!" )
+
+// ============== Q-4 =============
+// alert("Welcome To Js Land...")
+// alert("Happy Coding!")
