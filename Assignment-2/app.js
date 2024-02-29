e436d841a78fabae567cbb8aa5895efc6d2bcab1
ahsansamad <ingeniouslogo@gmail.com>
2024-02-29 15:17:39 +0500

    chapter-5-assignment-completed

---
+++ Assignment-2/app.js
@@ -0,0 +1,63 @@
+// ****************** Chapter-4 ****************
+// ==== Q-1 ====
+// var statement = a = 10, b = 20, c = 30;
+// console.log(a,b,c)
+
+// ==== Q-2 ====
+// var legalName = myVar, my_name, $ahsan, _ahsan , ahsan;
+// var illegalName = 3Myvar, Ahsan, var , alert, console ;
+
+// ==== Q-3 ====
+// document.write("A heading stating “Rules for naming JS variables” </br>")
+// document.write("'A'.Variable names can only contain $sign, _underscore, letters  and numbers. </br>")
+// document.write("'B' .Variables must begin with a smallLetters, numbers or $sign and _underScore.")
+// document.write("'C' .Variable Names Are Case Sensitive.</br>");
+// document.write("'D' .Variable names should not be JS Predefined Keywords.");
+// ****************** Chapter-4-End ****************
+
+// ****************** Chapter-5 ****************
+// ==== Q-1 ====
+// var incr = 3 + 5
+// var sum = incr
+// console.log(sum)
+
+// ==== Substract ====
+// var subs = 5 - 3
+// var sum = subs 
+// console.log(sum)
+
+// ==== Multi ====
+// var mult = 5 * 3
+// var sum = mult 
+// console.log(sum)
+
+// ==== Division ====
+// var div = 5 / 3
+// var sum = div 
+// console.log(sum)
+
+// ==== Modulus / Reminder ====
+// var rem = 10 % 3;
+// var sum = rem ;
+// console.log(sum)
+
+
+
+
+
+
+// ==== Q-5 ====
+
+// var table = "Table of 4 <br><br>";
+// document.write(table)
+// var four1 = 4;
+// document.write("4*1=", four1, "<br>");
+// document.write("4*2=", four1 * 2, "<br>");
+// document.write("4*3=", four1 * 3, "<br>");
+// document.write("4*4=", four1 * 4, "<br>");
+// document.write("4*5=", four1 * 5, "<br>");
+// document.write("4*6=", four1 * 6, "<br>");
+// document.write("4*7=", four1 * 7, "<br>");
+// document.write("4*8=", four1 * 8, "<br>");
+// document.write("4*9=", four1 * 9, "<br>");
+// document.write("4*10=", four1 * 10, "<br>");
