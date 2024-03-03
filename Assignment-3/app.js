854426cc374d4caa113b8d05502de49069f9a44d
ahsansamad <ingeniouslogo@gmail.com>
2024-03-04 01:10:33 +0500

    report-card-created

--- Assignment-3/app.js
+++ Assignment-3/app.js
@@ -89,3 +89,31 @@
 // if ("car" < "cat") {
 //   alert("car is smaller than cat");
 // }
+
+// ==== Q-6 ====
+// var totalMarks1 = +prompt("what are the total marks of each subject") * 3;
+// var subject1 = prompt("enter Subject one");
+// var subject1Marks = +prompt("enter how many marks you obtained in " + subject1);
+
+// var subject2 = prompt("enter Subject two");
+// var subject2Marks = +prompt("enter how many marks you obtained in " + subject2);
+
+// var subject3 = prompt("enter Subject three");
+// var subject3Marks = +prompt("enter how many marks you obtained in " + subject3);
+// var percentage =
+//   ((subject1Marks + subject2Marks + subject3Marks) * 100) / totalMarks1;
+// var totalObtained = subject1Marks + subject2Marks + subject3Marks;
+// var grade = "";
+// if (percentage >= 80) {
+//   grade = "A-One " + " " + "Excellent";
+// } else if (percentage >= 70) {
+//   grade = "A " + " " + "Very Good";
+// } else if (percentage >= 60) {
+//   grade = "B " + " " + "you need imporvement";
+// } else {
+//   grade = "fail" + " " + "Sorry";
+// }
+// document.write("<b>" + " " + "Total Marks " + totalMarks1 + "</b></br>");
+// document.write("<b>" + " " + "Marks Obtained " + totalObtained + "</b></br>");
+// document.write("<b>" + " " + "Percentage " + percentage + "%" + "</b></br>");
+// document.write("<b>" + " " + "Grade: " + grade + "</b></br>");
