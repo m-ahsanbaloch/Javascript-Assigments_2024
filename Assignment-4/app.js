185f9ffbf2594a2f576fef9658b0f105c075f643
ahsansamad <ingeniouslogo@gmail.com>
2024-03-13 06:40:46 +0500

    assignment-4

--- Assignment-4/app.js
+++ Assignment-4/app.js
@@ -1,1 +1,20 @@
-var eArr = []
+// ********** Chapter - 14 - 16 **********
+// // === Q-1 ===
+// var eArr = []
+// // === Q-2 ===
+// var students = []
+// // === Q-3 ===
+// var sArr = ["ahsan"]
+// // === Q-4 ===
+// var nArr = [1244]
+// // ===  Q-5 ====
+// var bArr = [true,false]
+// // ===  Q-6 ===
+// var mArr = ["string",1234,true]
+// // ===  Q-7 ===
+// var qualification = ["SSC", "HSC", "BCS","BS", "BCOM", "MS", "M.", "Phil.", "PhD"]
+// ===  Q-8 ===
+var sNames = ["michael","john","tony"];
+var sScores = [320 , 230 , 480];
+var student1 = document.write(sNames[0] + " is" + " " + sScores[0] + " " + sScores[0] * 100 / 500 + "%" + "</br>")
+var student2 = document.write(sNames[1] + " is" + " " + sScores[1] + " " + sScores[1] * 100 / 500 + "%" + "</br>")
