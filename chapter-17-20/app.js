f562b92d41f37429fc4b733036cd310c02d084bf
ahsansamad <ingeniouslogo@gmail.com>
2024-03-17 05:58:00 +0500

    array of arrays

---
+++ chapter-17-20/app.js
@@ -0,0 +1,14 @@
+// ==== (Q-1) ===
+// var multiArr = [[],[],[]]
+// console.log(multiArr)
+
+// ==== (Q-2) ===
+// var multiArr = [["0123"],["1012"],["2101"]]
+// document.write(`${multiArr[0]} </br> ${multiArr[1]} </br> ${multiArr[2]}`);
+
+// ==== (Q-3) ===
+// for(var i = 1; i <= 10; i++){
+// document.write(`<b>${i}</b> </br>`)
+// }
+
+// ==== (Q-4) ===
