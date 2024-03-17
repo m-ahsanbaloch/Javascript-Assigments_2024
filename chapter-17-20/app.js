11e2edf5995c8484a58379783037fe6897c0e16d
ahsansamad <ingeniouslogo@gmail.com>
2024-03-17 06:18:25 +0500

    changes

--- chapter-17-20/app.js
+++ chapter-17-20/app.js
@@ -12,3 +12,19 @@
 // }
 
 // ==== (Q-4) ===
+// var tableOf = +prompt("Enter which table you want to show in browser");
+// var tableLength = +prompt("enter table length you want to see in browser");
+// for(var i = 1; i <= tableLength;i++){
+//     document.write(`${tableOf} x ${i} = ` + tableOf * i +  "</br>")
+// }
+
+// ==== (Q-5) ===
+// var fruits = ["“apple”", "“banana”", "“mango”", "“orange”","“strawberry”"];
+// for(var i = 0; i < fruits.length; i++){
+//     document.write(`${fruits[i] }</br>`)    
+// }
+// document.write(`</br>Element at index 0 is ${fruits[0]} </br>
+// Element at index 1 is ${fruits[1]} </br>
+// Element at index 2 is ${fruits[2]} </br>
+// Element at index 3 is ${fruits[3]} </br>
+// Element at index 4 is ${fruits[4]} </br>`)
