b0dd3c961083c06ac741e6550e7d1ce85bc51afc
ahsansamad <ingeniouslogo@gmail.com>
2024-03-18 21:44:07 +0500

    searching completed

--- chapter-21-25/app.js
+++ chapter-21-25/app.js
@@ -3,22 +3,20 @@
 // var fName = "Mohammad"
 // var lName = "Ahsan"
 // var fullName = `${fName} ${lName}`;
-// document.write(fullName); // Mohammad Ahsan 
+// document.write(fullName); // Mohammad Ahsan
 
 // ==== (Q-2) ====
 
-var apple = "iphone 15 pro max"
-var samsung = " samsung galaxy s6"
-var phones = apple + samsung
-var fPhone = prompt("enter your favourite phone");
-var check = false
-for(var i = 0; i < phones.length; i++){
-    if(phones.slice(i,i + fPhone.length) === fPhone){
-        check = true
-        document.write(`my favourite phones is ${fPhone} </br>`)
-        document.write(`index is ${phones.indexOf(fPhone)}`)
-    }
-}
-if(check === false){
-    document.write(`Sorry this device is not available right now`)
-}
+// var phones = "iphone 15 pro max, samsung galaxy s24 ultra, vivo, oppo";
+// var fPhone = prompt("enter your favourite phone");
+// var check = false;
+// for (var i = 0; i < phones.length; i++) {
+//   if (phones.slice(i, i + fPhone.length) === fPhone) {
+//     check = true;
+//     document.write(`my favourite phones is ${fPhone} </br>`);
+//     document.write(`index is ${phones.indexOf(fPhone)}`);
+//   }
+// }
+// if (check === false) {
+//   document.write(`Sorry this device is not available right now`);
+// }
