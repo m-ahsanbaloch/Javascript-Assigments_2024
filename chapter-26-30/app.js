f79729c53afd2c153bf4354388b577ffa90f0e19
ahsansamad <ingeniouslogo@gmail.com>
2024-03-23 05:47:44 +0500

    math

---
+++ chapter-26-30/app.js
@@ -0,0 +1,8 @@
+var user = 3.45214
+document.write(`${user} <br>`)
+
+document.write(`round of value: ${Math.round(user)}<br>`)
+
+document.write(`Floor value: ${Math.floor(user)}<br>`)
+
+document.write(`ceil value: ${Math.ceil(user)}`)
