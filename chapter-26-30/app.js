9eb3ae47dc09c43f259d5bd64f4ab46383bce313
ahsansamad <ingeniouslogo@gmail.com>
2024-03-23 06:50:41 +0500

    math absolute

--- chapter-26-30/app.js
+++ chapter-26-30/app.js
@@ -1,8 +1,18 @@
-var user = 3.45214
-document.write(`${user} <br>`)
+// ==== (Q-1) ====
 
-document.write(`round of value: ${Math.round(user)}<br>`)
+// var user = 3.45214
+// document.write(`${user} <br>`)
 
-document.write(`Floor value: ${Math.floor(user)}<br>`)
+// document.write(`round of value: ${Math.round(user)}<br>`)
 
-document.write(`ceil value: ${Math.ceil(user)}`)
+// document.write(`Floor value: ${Math.floor(user)}<br>`)
+
+// document.write(`ceil value: ${Math.ceil(user)}`)
+
+// ==== (Q-2) =====
+// var num = -2.673
+// document.write(`number: ${num} <br> round of value ${Math.round(num)} <br> floor value ${Math.floor(num) } <br> ceil value ${Math.ceil(num)}`)
+
+// ==== (Q-3) ====
+// var num = -6
+// document.write(`the absolute value of ${num} is : ${Math.abs(num)}`)
