64e5032ece8ccde18b7faf2fcc38daa60669c51d
ahsansamad <ingeniouslogo@gmail.com>
2024-03-24 21:45:12 +0500

    months created

--- chapter-31-34/app.js
+++ chapter-31-34/app.js
@@ -1,4 +1,14 @@
 // ==== (Q-1) ====
 
-var date = new Date()
-document.write(rightnow)
+// var date = new Date();
+// document.write(date);
+
+// ==== (Q-2) ====
+
+var months = ["jan","feb","march","april","may","june","july","august","sep","oct","nov","dec"]
+
+   var date = new Date()
+   var month = date.getMonth()
+   months = months[month]
+    document.write(months)
+
