5f7d3147b0b50aef1bf92dc9afab2b35308b35f1
ahsansamad <ingeniouslogo@gmail.com>
2024-03-29 21:48:20 +0500

    set

--- password generator/app.js
+++ password generator/app.js
@@ -1,20 +1,22 @@
-var small = "abcdefghijklmnopqrstuvwxyz"
-let smallPassword = ""
-for(i = 0 ; i < 4; i++){
-    randomValue = Math.floor(Math.random() * small.length)
-    smallPassword += small[randomValue]
-}
+// var small = "abcdefghijklmnopqrstuvwxyz"
+// let smallPassword = ""
+// for(i = 0 ; i < 4; i++){
+//     randomValue = Math.floor(Math.random() * small.length)
+//     smallPassword += small[randomValue]
+// }
 
-var cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-let capPass = ""
-for(i = 0 ; i < 4; i++){
-    randomValue = Math.floor(Math.random() * cap.length)
-    capPass += cap[randomValue]
-}
- var sym = "!@#$%^&*("
- let symPass = ""
- for(i = 0 ; i <4; i++){
-     randomValue = Math.floor(Math.random() * sym.length)
-     symPass += sym[randomValue]
- }
- document.write(`your password is ${smallPassword + capPass + symPass }`);
+// var cap = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// let capPass = ""
+// for(i = 0 ; i < 4; i++){
+//     randomValue = Math.floor(Math.random() * cap.length)
+//     capPass += cap[randomValue]
+// }
+//  var sym = "!@#$%^&*("
+//  let symPass = ""
+//  for(i = 0 ; i <4; i++){
+//      randomValue = Math.floor(Math.random() * sym.length)
+//      symPass += sym[randomValue]
+//  }
+//  document.write(`your password is ${smallPassword + capPass + symPass }`);
+
+ 
