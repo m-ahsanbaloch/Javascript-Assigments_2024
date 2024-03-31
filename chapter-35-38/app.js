a52e893016e6a9e4537a519e03b08707d895e07f
ahsansamad <ingeniouslogo@gmail.com>
2024-03-31 20:38:34 +0500

    fullname created

--- chapter-35-38/app.js
+++ chapter-35-38/app.js
@@ -1,7 +1,16 @@
 // ==== (Q-1) ====
-function date(){
-    var today = new Date()
-    document.write(`${today}`)
-}
+// function date(){
+//     var today = new Date()
+//     document.write(`${today}`)
+// }
 
-date();
+// date();
+
+// var firstName = "Ahsan";
+// var lastName = "Baloch";
+
+// function fullname(){
+//     var fullName = firstName + " " + lastName
+//     document.write(fullName);
+// }
+// fullname();
